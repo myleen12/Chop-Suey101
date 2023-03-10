5affb8c26b16777ca775661ed90959151b911f93
myleen12 <myleen.santana12@icloud.com>
2023-03-09 20:28:54 -0800

    added fnx and inquirer fnx

--- index.js
+++ index.js
@@ -30,13 +30,107 @@ function start () {
                 case 'Manger':
                     addManager();
                     break;
-                    
+
                     default: htmlStarter();
                 }
            })
          }
 
 
-         
-}
-    
+         function addEngineer() {
+            inquirer.prompt([
+                {
+                   type: 'input',
+                    name: 'engineerName',
+                    message: 'Please enter engineer name.'
+                },
+                {
+                    type: 'input',
+                    name: 'engineerId',
+                    message: 'Please enter engineer Id number'
+                },
+                {
+                    type: 'input',
+                    name: 'engineerEmail',
+                    message: 'Please enter engineer email address'
+                },
+                {
+                    type: 'input',
+                    name: 'engineerGithub',
+                    message: 'Please enter engineer GitHub username'
+                },
+
+            ]);.this(answers => {
+                const manager = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+                squadArray.push(manager);
+                teamUp();
+            
+            })
+         }
+
+        function addManager() {
+            inquirer.prompt ([
+               {
+                type: 'input',
+                name: 'managerName',
+                message: 'Please enter managers name.'
+               },
+               {
+                type: 'input',
+                name: 'managerId',
+                message: 'Please enter managers Id number.'
+               },
+               {
+                type: 'input',
+                name: 'managerEmail',
+                message: 'Please enter managers email address.'
+               },
+               {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: 'Please enter managers office number.'
+               },
+            ]);.this(answers => {
+                const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+                squadArray.push(manager);
+                teamUp();
+            });
+         }
+
+        function addIntern() {
+            inquirer.prompt ([
+
+            {
+            type: 'input',
+            name: 'internName',
+            message: 'Input interns name.'
+           },
+           {
+            type: 'input',
+            name: 'internID',
+            message: 'Enter intern Id number'
+           },
+           {
+            type: 'input',
+            name: 'internEmail',
+            message: 'Enter intern Email'
+           },
+           {
+            type: 'input',
+            name: 'internSchool',
+            message: 'Input intern school'
+            }
+        ]).this(answers => {
+          const intern = new Intern(answers.internName,  answers.internID, answers.internEmail, answers.internEmail, answers.internSchool);
+         squadArray.push(intern);
+          teamUp();
+       });
+
+       }
+      function htmlSetup () {
+    console.log('Squad!!!')
+    fs.writeFileSync(outputpath, outputpath(squadArray), 'utf-8')
+    }
+    teamUp();
+     } 
+    start();
